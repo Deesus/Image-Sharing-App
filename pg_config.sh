d912d0d3d48709631e37fc4e6fa8b461b19d5213
Dee <d.reddy@yandex.com>
2015-12-01 12:56:21 -0500

    refactor: Replace setup.sh with Vagrantfile
    The setup.sh script was unnecessary. Included Vagrant file as well as
    pg_config. This reduces number of steps for end-user.

--- pg_config.sh
+++ pg_config.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
 apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
 pip install oauth2client
 pip install requests
+pip install httplib2
 pip install flask-seasurf
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
+su vagrant -c 'psql -f /vagrant/Image-Sharing-App/create_database.sql'
+
+vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
+echo -e $vagrantTip > /etc/motd
+
 printf "\n\n\n"
 echo "******  $(tput setaf 2)Setup complete$(tput setaf 7)  ******"
 echo "Run $(tput setab 4)psql -f create_database.sql$(tput setab 0)"
